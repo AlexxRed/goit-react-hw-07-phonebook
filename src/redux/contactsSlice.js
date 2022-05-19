fd759c67bd47b38e7f44dc2325242d1a8c7e3752
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-05-19 15:17:39 +0300

    delete persist

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,13 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-// const initialContacts = [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ] 
 
 
 
@@ -29,7 +22,6 @@ export const contactsSlice = createSlice({
             state.items.push(action.payload);
         },
         removeContact(state, action) {
-            // state.items.filter(contact => contact.id !== action.payload);
             state.items = state.items.filter(
             ({ id }) => id !== action.payload
             );
@@ -40,12 +32,7 @@ export const contactsSlice = createSlice({
     }
 });
 
-const persistConfig = {
-    key: 'contacts',
-    storage,
-};
-export const contactsReducer = persistReducer(
-    persistConfig,
+export const contactsReducer = (
     contactsSlice.reducer
 );
 
@@ -54,38 +41,3 @@ export const getContactsList = ({ contacts }) => contacts.items;
 export const getFilteredContacts = ({ contacts }) => contacts.filter;
 
 
-
-
-
-
-
-
-
-
-// const increment = createAction ('clicks/increment');
-
-// const clickReducer = createReducer(0, {
-//     [increment]: (state, action) => {
-//         // return {value: state.value + action.payload,}
-//         state.value += action.payload
-//     }
-// });
-// const initialState = { value: 0 };
-
-
-// export const clickSlice = createSlice({
-//     name: 'clicks',
-//     initialState,
-//     reducers: {
-//         increment(state, action) {
-//             state.value += action.payload;
-//         },
-//         reset(state) {
-//             state.value = 0;
-//         }
-//     }
-// });
-
-// export const { increment, reset } = clickSlice.actions;
-
-// export const getClicks = state => state.clicks.value;
